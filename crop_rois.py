d20a137cc60466866d892076d2a65e92fffcc604
Martin Sivak <xsivakm1@stuba.sk>
2024-10-24 16:20:40 +0200

    git: edit gitignore

--- crop_rois.py
+++ crop_rois.py
@@ -16,6 +16,7 @@ def image_rois_from_coco_json(json_path: str, image_dir: str, output_dir: str, l
         image_path = os.path.join(image_dir, image_name)
         rois = [ann['bbox'] for ann in data['annotations'] if ann['image_id'] == image_id]
         crop_image_rois(image_path=image_path, regions=rois, output_dir=output_dir)
+        print(f"Processed image {image_name}")
         limit -= 1
         if limit <= 0:
             return
@@ -31,7 +32,7 @@ def crop_image_rois(image_path: str, output_dir: str, regions: list[list[int, in
         roi = image.crop((x, y, x + width, y+height))
 
         # Store each ROI into desired directory
-        roi.save(os.path.join(output_dir, f"{image_name}_{x}_{y}_{width}_{height}.png"))
+        roi.save(os.path.join(output_dir, f"{image_name}_-_{x}_{y}_{width}_{height}.png"))
 
 
 
